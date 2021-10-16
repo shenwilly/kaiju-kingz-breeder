381e6d6b3ead7121f3436e2f2a080f0b80d4356e
willy <shen.willy95@gmail.com>
2021-10-16 10:42:27 +0700

    feat: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import hre from "hardhat";
+import { KAIJUKINGZ_ADDRESS } from "../constants";
+import { KaijuKingzBreeder__factory } from "../typechain";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const BreederFactory = <KaijuKingzBreeder__factory>(
+    await hre.ethers.getContractFactory("KaijuKingzBreeder")
+  );
+  const breeder = await BreederFactory.deploy(KAIJUKINGZ_ADDRESS);
+  await breeder.deployed();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello");
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Breeder deployed to:", breeder.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
